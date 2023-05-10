1c9d1d6bc60708f428d450c0c611bd56e01431bf
Mirza Sahin <salihmirza123@gmail.com>
2023-05-10 21:11:38 +0300

    Add global styled components

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,18 +1,16 @@
 import Link from "next/link";
 import Header from "./Header";
-import { styled, createGlobalStyle } from "styled-components";
+import { styled } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
-body{
-}
-
+const BodyDiv = styled.div`
+font-size: 20px;
 `
 
 export default ({children}) => (
-  <GlobalStyle>
+  <BodyDiv>
     <Header />
     <h2 style={{backgroundColor:'tomato', width:"100vh", margin:"10px", padding:"10px"}}>{children}</h2>
     <footer> Powered Mirza Sahin</footer>
 
-  </GlobalStyle>
+  </BodyDiv>
 );
---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+ import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body{
+margin: 0;
+padding: 0;
+}
+`;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+        <GlobalStyle />
+        <Component {...pageProps} />
+    </>
+  )
+}
